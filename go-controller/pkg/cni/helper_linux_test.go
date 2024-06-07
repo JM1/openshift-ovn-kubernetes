207c1f442df2fa6e59060047f0ec30b5f22f8fe6
Jakob Meng <code@jakobmeng.de>
2024-06-07 09:48:37 +0200

    Use VDUSE instead of Veth devices with OVS' netdev (userspace) datapaths

--- go-controller/pkg/cni/helper_linux_test.go
+++ go-controller/pkg/cni/helper_linux_test.go
@@ -516,7 +516,7 @@ func TestSetupInterface(t *testing.T) {
 			ovntest.ProcessMockFnList(&mockCNIPlugin.Mock, tc.cniPluginMockHelper)
 			ovntest.ProcessMockFnList(&mockNS.Mock, tc.nsMockHelper)
 
-			hostIface, contIface, err := setupInterface(tc.inpNetNS, tc.inpContID, tc.inpIfaceName, tc.inpPodIfaceInfo)
+			hostIface, contIface, err := setupVethInterface(tc.inpNetNS, tc.inpContID, tc.inpIfaceName, tc.inpPodIfaceInfo)
 			t.Log(hostIface, contIface, err)
 			if tc.errExp {
 				assert.NotNil(t, err)
@@ -1526,7 +1526,7 @@ func TestConfigureOVS(t *testing.T) {
 			fakeClient := fake.NewSimpleClientset(&v1.PodList{Items: []v1.Pod{pod}})
 			clientset := NewClientSet(fakeClient, &podLister)
 			err = ConfigureOVS(ctx, tc.podNs, tc.podName, tc.vfRep,
-				tc.ifInfo, sandboxID, vfPciAddress, clientset)
+				tc.ifInfo, sandboxID, vfPciAddress, clientset, ovntypes.DatapathSystem)
 			if tc.errMatch != nil {
 				assert.Contains(t, err.Error(), tc.errMatch.Error())
 			} else {
@@ -1661,7 +1661,7 @@ func TestConfigureOVS_getPfEncapIpWithError(t *testing.T) {
 			var podLister v1mocks.PodLister
 			podLister.On("Pods", mock.AnythingOfType("string")).Return(&podNamespaceLister)
 			err = ConfigureOVS(ctx, tc.podNs, tc.podName, tc.vfRep,
-				tc.ifInfo, sandboxID, vfPciAddress, nil)
+				tc.ifInfo, sandboxID, vfPciAddress, nil, ovntypes.DatapathSystem)
 			if tc.errMatch != nil {
 				assert.Contains(t, err.Error(), tc.errMatch.Error())
 			} else {
